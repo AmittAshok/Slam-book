d28ac86913e4fcb2aea6329bf70fd1856220dd8e
AmittAshok <124657793+AmittAshok@users.noreply.github.com>
2024-07-31 15:34:44 -0400

    Create app.py
    added code

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
+
+app = Flask(__name__)
+
+# MySQL configurations
+db = mysql.connector.connect(
+    host="localhost",
+    user="your_username",
+    password="your_password",
+    database="user_info"
+)
+
+cursor = db.cursor()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    dob = request.form['dob']
+    blood_group = request.form['blood-group']
+    location = request.form['location']
+    hobbies = request.form['hobbies']
+
+    query = "INSERT INTO users (name, dob, blood_group, location, hobbies) VALUES (%s, %s, %s, %s, %s)"
+    values = (name, dob, blood_group, location, hobbies)
+    cursor.execute(query, values)
+    db.commit()
+
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
